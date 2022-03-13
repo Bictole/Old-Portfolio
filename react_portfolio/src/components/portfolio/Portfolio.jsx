b52ae393a609b49ffd693547ccc7b48ce3bf97a0
Victor Simonin <vsimonin@lrde.epita.fr>
2022-03-13 22:21:33 +0100

    [Refacto] Project in portolio mapped from array

--- react_portfolio/src/components/portfolio/Portfolio.jsx
+++ react_portfolio/src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,51 @@ import React from 'react'
 import './portfolio.css'
 import ODYH from '../../assets/odyh.png'
 
+const data = [
+  {
+    id: 1,
+    image: ODYH,
+    title: 'The Odyh Project',
+    github: 'https://github.com/Bictole',
+    demo : 'https://github.com/Bictole'
+  },
+  {
+    id: 2,
+    image: ODYH,
+    title: 'The Odyh Project',
+    github: 'https://github.com/Bictole',
+    demo : 'https://github.com/Bictole'
+  },
+  {
+    id: 3,
+    image: ODYH,
+    title: 'The Odyh Project',
+    github: 'https://github.com/Bictole',
+    demo : 'https://github.com/Bictole'
+  },
+  {
+    id: 4,
+    image: ODYH,
+    title: 'The Odyh Project',
+    github: 'https://github.com/Bictole',
+    demo : 'https://github.com/Bictole'
+  },
+  {
+    id: 5,
+    image: ODYH,
+    title: 'The Odyh Project',
+    github: 'https://github.com/Bictole',
+    demo : 'https://github.com/Bictole'
+  },
+  {
+    id: 6,
+    image: ODYH,
+    title: 'The Odyh Project',
+    github: 'https://github.com/Bictole',
+    demo : 'https://github.com/Bictole'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -9,18 +54,23 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <container className="portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={ODYH} alt="Odyh project" />
-          </div>
-
-          <h3>Portfolio Title</h3> 
-
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/Bictole" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com/Bictole" className='btn btn-primary' target='_blank'>Demo</a>
-          </div>
-        </article>        
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return(
+            <article key={id} className='portfolio__item'>
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+  
+              <h3>{title}</h3> 
+  
+              <div className="portfolio__item-cta">
+                <a href={github} className='btn' target='_blank'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Demo</a>
+              </div>
+            </article>)
+          })
+        }        
       </container>
     </section>
   )
