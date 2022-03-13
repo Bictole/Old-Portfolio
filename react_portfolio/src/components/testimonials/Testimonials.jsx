37a810e5bfc263e64d6099b81567103aa25b977e
Victor Simonin <vsimonin@lrde.epita.fr>
2022-03-13 22:45:48 +0100

    [Add] Testimonial component and styles

--- react_portfolio/src/components/testimonials/Testimonials.jsx
+++ react_portfolio/src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './testimonials.css'
+import ALEXANDRE from '../../assets/alexandre.png'
+
+const data = [
+  {
+    avatar: ALEXANDRE,
+    name: 'Alexandre Lemonnier',
+    review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae architecto dicta maxime necessitatibus vero fuga eveniet quod, magnam dolorum illum praesentium obcaecati voluptatum exercitationem debitis facere omnis provident est. Porro.'
+  },
+  {
+    avatar: ALEXANDRE,
+    name: 'Alexandre Lemonnier',
+    review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae architecto dicta maxime necessitatibus vero fuga eveniet quod, magnam dolorum illum praesentium obcaecati voluptatum exercitationem debitis facere omnis provident est. Porro.'
+  },
+  {
+    avatar: ALEXANDRE,
+    name: 'Alexandre Lemonnier',
+    review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae architecto dicta maxime necessitatibus vero fuga eveniet quod, magnam dolorum illum praesentium obcaecati voluptatum exercitationem debitis facere omnis provident est. Porro.'
+  },
+  {
+    avatar: ALEXANDRE,
+    name: 'Alexandre Lemonnier',
+    review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae architecto dicta maxime necessitatibus vero fuga eveniet quod, magnam dolorum illum praesentium obcaecati voluptatum exercitationem debitis facere omnis provident est. Porro.'
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Some Reviews</h5>
+      <h2>Tesimonials</h2>
+
+      <div className="container testimonials__container">
+        {
+          data.map(({avatar, name, review}, index) =>{
+            return(
+            <article key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className='client__name'>{name}</h5>
+              <small className='client__review'>{review}</small>
+            </article>)
+          })
+        }
+
+      </div>
+    </section>
   )
 }
 
