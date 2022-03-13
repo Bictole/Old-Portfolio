c5c51fee97fcbcbb299068f52892584c9a9391e6
Victor Simonin <vsimonin@lrde.epita.fr>
2022-03-13 22:08:58 +0100

    [Add] Portfolio coomponent and style

--- react_portfolio/src/components/portfolio/Portfolio.jsx
+++ react_portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './portfolio.css'
+import ODYH from '../../assets/odyh.png'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <container className="portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={ODYH} alt="Odyh project" />
+          </div>
+
+          <h3>Portfolio Title</h3> 
+
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/Bictole" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com/Bictole" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>        
+      </container>
+    </section>
   )
 }
 
