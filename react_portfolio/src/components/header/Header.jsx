f706eccc178711f00af80c876c9916d568942b92
Victor Simonin <vsimonin@lrde.epita.fr>
2022-03-13 01:00:19 +0100

    [Add] Update dependencies and complete Header

--- react_portfolio/src/components/header/Header.jsx
+++ react_portfolio/src/components/header/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/face.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello i'm</h5>
+        <h1>Victor</h1>
+        <h5 className="text-light">
+          Student At EPITA
+        </h5>
+        <CTA/>
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
